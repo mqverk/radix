cb76f52814f895db607146e8f46e707a86f3f261
mqverk <mqverk@gmail.com>
2026-03-18 23:48:40 +0530

    feat: implement top-k nearest neighbors function

--- src/indexes/math/topk.py
+++ src/indexes/math/topk.py
@@ -1,1 +1,13 @@
+import heapq
+
+def top_k(elements, k):
+    """
+    Retrieve the top-k elements from a list.
+
+    :param elements: List of tuples (value, data).
+    :param k: Number of top elements to retrieve.
+    :return: List of top-k elements.
+    """
+    return heapq.nlargest(k, elements, key=lambda x: x[0])
+
 # Placeholder for top-k nearest neighbors implementation
