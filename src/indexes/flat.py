606d656fcfac5ac0bb69113b85c447040da0bc5a
mqverk <mqverk@gmail.com>
2026-03-18 23:49:00 +0530

    feat: implement flat index for brute-force search

--- src/indexes/flat.py
+++ src/indexes/flat.py
@@ -1,1 +1,23 @@
-# Placeholder for flat index implementation
+from .math import euclidean_distance
+
+class FlatIndex:
+    """
+    Implements a flat index for brute-force search.
+    """
+
+    def __init__(self):
+        self.data = []
+
+    def add(self, vector):
+        """
+        Add a vector to the index.
+        """
+        self.data.append(vector)
+
+    def search(self, query, k):
+        """
+        Perform a brute-force search to find the top-k nearest neighbors.
+        """
+        distances = [(euclidean_distance(query, vec), vec) for vec in self.data]
+        distances.sort(key=lambda x: x[0])
+        return distances[:k]
